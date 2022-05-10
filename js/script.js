aa8624fbb770e950145665e56485d004ef4bf657
n4huerules <rulesnahue@gmail.com>
2022-05-10 12:49:13 -0300

    mi primer jocke

---
+++ js/script.js
@@ -0,0 +1,3 @@
+alert("robando sus datos... , si cierra la ventana no importa");
+alert("datos robados con exito, su sistema ha sido infectado");
+alert("tarjetas de credito, mails y todos sus datos ahora estan en nuestras manos, que tenga buen día");
